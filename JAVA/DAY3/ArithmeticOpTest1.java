eece8f36fc9f88f96aba4916c3e40e6aebb2386c
건다 <43940462+DY29@users.noreply.github.com>
2020-11-01 00:16:33 +0900

    Add files via upload
    산술연산자

---
+++ JAVA/DAY3/ArithmeticOpTest1.java
@@ -0,0 +1,18 @@
+public class ArithmeticOpTest1 {
+    public static void main(String ard[]) {
+        int num1 = 3;
+        int num2= 20;
+        int num3, num4, num5, num6, num7;
+        num3 = num2 + num1;
+        num4 = num2 - num1;
+        num5 = num2 * num1;
+        num6 = num2 / num1;
+        num7 = num2 % num1;
+
+        System.out.println("20 + 3 = " + num3);
+        System.out.println("20 - 3 = " + num4);
+        System.out.println("20 * 3 = " + num5);
+        System.out.println("20 / 3 = " + num6);
+        System.out.println("20 % 3 = " + num7);
+    }
+}
---
+++ JAVA/DAY3/ArithmeticOpTest2.java
@@ -0,0 +1,8 @@
+public class ArithmeticOpTest2 {
+    public static void main(String ard[]){
+        int num=3;
+        System.out.println("++3 = "+ ++num); // 1 + num
+        System.out.println("3++ = "+ num++); // num + 1
+        System.out.println(num);
+    }
+}
